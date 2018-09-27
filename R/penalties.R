27505063274a0bd638692f99ab2db56bcaee195a
Gavin Simpson <ucfagls@gmail.com>
2018-09-27 15:11:26 -0230

    use add_delta not delta; clean up; code style

--- R/penalties.R
+++ R/penalties.R
@@ -7,10 +7,10 @@
   type <- match.arg(type)
   node_labels <- levels(object)
   n_levels <- length(node_labels)
-  if(type == "full"){
+  if (type == "full") {
     pen <- matrix(-1, n_levels, n_levels)
-    diag(pen) <- n_levels - 1 + delta
-  } else if(type =="individual"){
+    diag(pen) <- n_levels - 1 + add_delta
+  } else if (type =="individual") {
     pen <- diag(1, n_levels)
   }
   types <-  c("full","individual")
@@ -21,43 +21,43 @@
   pen
 }
 
-`mrf_penalty.numeric` <- function(object,type = c("linear","cyclic"), node_labels = NULL, delta = 0,
+`mrf_penalty.numeric` <- function(object,type = c("linear","cyclic"), node_labels = NULL, add_delta = FALSE,
                                         end_points = NULL, ...){
   check_delta(add_delta)
-  type <-  match.arg(type)
+  type <- match.arg(type)
   object2 <- object
   object <- object[!duplicated(object)]
   n <-  length(object)
 
-  if(is.null(node_labels)){
+  if (is.null(node_labels)) {
     node_labels = as.character(object)
-  } else{
-    if(n != length(node_labels)){
+  } else {
+    if (n != length(node_labels)) {
       stop("object and lables need to be the same length")
     }
-    if(length(unique(node_labels))!=n){
+    if (length(unique(node_labels)) != n) {
       stop("all levels have to be unique")
     }
   }
 
-  indices = match(object, sort(object))
-  object = sort(object)
-  loc_diff = diff(object)
+  indices <- match(object, sort(object))
+  object <- sort(object)
+  loc_diff <- diff(object)
 
-  loc_diff_1 = c(Inf, loc_diff)
-  loc_diff_2 = c(loc_diff, Inf)
-  pen = diag(1/loc_diff_1 + 1/loc_diff_2 + delta)
-  diag(pen[-1,-n]) = diag(pen[-n,-1]) = -1/loc_diff
-  if(type=="circular"){
-    if(is.null(end_points)) {
+  loc_diff_1 <- c(Inf, loc_diff)
+  loc_diff_2 <- c(loc_diff, Inf)
+  pen <- diag(1/loc_diff_1 + 1/loc_diff_2 + add_delta)
+  diag(pen[-1, -n]) <- diag(pen[-n, -1]) <- -1 / loc_diff
+  if (type=="circular") {
+    if (is.null(end_points)) {
       end_points = c(min(object) - 1e-6, max(object) + 1e-6)
     }
-    dist_to_end = (object[1]-end_points[1]) + (end_points[2] - object[n])
-    pen[1,1] = pen[1,1] + 1/dist_to_end
-    pen[n,n] = pen[n,n] + 1/dist_to_end
-    pen[n,1] = pen[1,n] = -1/dist_to_end
+    dist_to_end = (object[1] - end_points[1]) + (end_points[2] - object[n])
+    pen[1, 1] <- pen[1, 1] + 1 / dist_to_end
+    pen[n, n] <- pen[n, n] + 1 / dist_to_end
+    pen[n, 1] <- pen[1, n] <- -1 / dist_to_end
   }
-  pen = pen[indices,indices]
+  pen <- pen[indices,indices]
 
   types <-  c("linear","cyclic")
   type_labels <- c("first_order_random_walk","cyclic_first_order_random_walk")

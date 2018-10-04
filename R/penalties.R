18c46a823daf6ac211cb7fe4923cbe3d0fbfe6e3
Gavin L. Simpson <ucfagls@gmail.com>
2018-10-04 10:58:00 -0600

    add a default method for mrf_penalty that fails gracefully if we don't handle a given class

--- R/penalties.R
+++ R/penalties.R
@@ -7,6 +7,15 @@
   UseMethod("mrf_penalty")
 }
 
+##' @export
+`mrf_penalty.default` <- function(object, ...) {
+    ## want to bail with a useful error;
+    ## see Jenny Bryan's Code Smells UseR 2018 talk: rstd.io/code-smells
+    stop("Unable to create an MRF penalty from <",
+         class(object)[[1L]], ">",
+         call. = FALSE)                 # don't show the call, simpler error
+}
+
 ##' @title Fully connected graph and random effect MRF penalties from a factor
 ##'
 ##' @param object an R object to create the MRF penalty from.

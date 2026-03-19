850c5aef37f1e676eb81d0e1c580a7053a5ea1a1
Gavin Simpson <ucfagls@gmail.com>
2026-03-19 14:59:36 +0100

    reformat cde according to air

--- R/utils.R
+++ R/utils.R
@@ -14,11 +14,14 @@
 
 #' @export
 `get_mrf.default` <- function(object, ...) {
-    ## want to bail with a useful error;
-    ## see Jenny Bryan's Code Smells UseR 2018 talk: rstd.io/code-smells
-    stop("Don't know how to extract an MRF penalty from <",
-         class(object)[[1L]], ">",
-         call. = FALSE)           # don't show the call, simpler error
+  ## want to bail with a useful error;
+  ## see Jenny Bryan's Code Smells UseR 2018 talk: rstd.io/code-smells
+  stop(
+    "Don't know how to extract an MRF penalty from <",
+    class(object)[[1L]],
+    ">",
+    call. = FALSE
+  ) # don't show the call, simpler error
 }
 
 #' @export
@@ -37,7 +40,7 @@
 #' @export
 #' @rdname get_mrf
 `get_mrf.list` <- function(object, ...) {
-  if(!"gam" %in% names(object)){
+  if (!"gam" %in% names(object)) {
     stop("Not a gamm or gamm4 object. No smooth object for get_mrf to use")
   }
   object <- object[["gam"]]
@@ -75,38 +78,45 @@
   list()
 }
 
-`mrf_config` <- function(type = NULL,
-                         node_labels = NULL,
-                         delta = NULL,
-                         geometry = NULL,
-                         phylogeny = NULL,
-                         random_walk = NULL,
-                         dendrogram = NULL,
-                         graph  = NULL,
-                         dissimiliarities = NULL) {
-    ## could return:
-    ## unique factor levels associated with the data
-    ## MRF type
-    ## information for plotting (e.g. the graph, phylogeny, etc.)
-    ## geographic information
-    config <- list(type = type,
-                   node_labels = node_labels,
-                   delta = delta,
-                   geometry = geometry,
-                   phylogeny = phylogeny,
-                   random_walk = random_walk,
-                   dendrogram = dendrogram,
-                   graph  = graph,
-                   dissimiliarities = dissimiliarities)
-    class(config) <- "mrf_config"
-    config
+`mrf_config` <- function(
+  type = NULL,
+  node_labels = NULL,
+  delta = NULL,
+  geometry = NULL,
+  phylogeny = NULL,
+  random_walk = NULL,
+  dendrogram = NULL,
+  graph = NULL,
+  dissimiliarities = NULL
+) {
+  ## could return:
+  ## unique factor levels associated with the data
+  ## MRF type
+  ## information for plotting (e.g. the graph, phylogeny, etc.)
+  ## geographic information
+  config <- list(
+    type = type,
+    node_labels = node_labels,
+    delta = delta,
+    geometry = geometry,
+    phylogeny = phylogeny,
+    random_walk = random_walk,
+    dendrogram = dendrogram,
+    graph = graph,
+    dissimiliarities = dissimiliarities
+  )
+  class(config) <- "mrf_config"
+  config
 }
 
-`as_mrf_penalty` <- function(penalty, config){
+`as_mrf_penalty` <- function(penalty, config) {
   #need to check if penalty is a matrix
   dimnames(penalty) <- list(config[["node_labels"]], config[["node_labels"]])
-  class(penalty) <-  c(paste0(config[["type"]], "_mrf_penalty"),
-    "mrf_penalty", "matrix")
+  class(penalty) <- c(
+    paste0(config[["type"]], "_mrf_penalty"),
+    "mrf_penalty",
+    "matrix"
+  )
   attr(penalty, which = "mrf_config") <- config
   penalty
 }
@@ -117,8 +127,8 @@
 #'
 #' @export
 `get_labels` <- function(penalty) {
-    config <- get_config(penalty)
-    config[["node_labels"]]
+  config <- get_config(penalty)
+  config[["node_labels"]]
 }
 
 #' @title Extract configuration details of an MRF penalty
@@ -129,7 +139,7 @@
 #'
 #' @export
 `get_config` <- function(penalty) {
-    attr(penalty, which = "mrf_config")
+  attr(penalty, which = "mrf_config")
 }
 
 #' @title Extract the type of MRF from the penalty
@@ -140,41 +150,44 @@
 #'
 #' @export
 `get_type` <- function(object) {
-    UseMethod("get_type")
+  UseMethod("get_type")
 }
 
 #' @export
 `get_type.default` <- function(object, ...) {
-    ## want to bail with a useful error;
-    ## see Jenny Bryan's Code Smells UseR 2018 talk: rstd.io/code-smells
-    stop("Don't know how to identify the type MRF penalty from <",
-         class(object)[[1L]], ">",
-         call. = FALSE)           # don't show the call, simpler error
+  ## want to bail with a useful error;
+  ## see Jenny Bryan's Code Smells UseR 2018 talk: rstd.io/code-smells
+  stop(
+    "Don't know how to identify the type MRF penalty from <",
+    class(object)[[1L]],
+    ">",
+    call. = FALSE
+  ) # don't show the call, simpler error
 }
 
 #' @export
 #' @rdname get_type
 `get_type.mrf_penalty` <- function(object) {
-    get_type(get_config(object))
+  get_type(get_config(object))
 }
 
 #' @export
 #' @rdname get_type
 `get_type.mrf_config` <- function(object) {
-    object[["type"]]
+  object[["type"]]
 }
 
 `check_delta` <- function(add_delta) {
-    if (length(add_delta) > 1) {
-        stop("'add_delta' has to be a single value, either logical or numeric")
-    }
-    if (!(is.logical(add_delta) || is.numeric(add_delta))) {
-        stop("'add_delta' has to be either logical or numeric")
-    }
-    if (is.numeric(add_delta) && add_delta < 0) {
-        stop("'add_delta' has to be zero or a positive number")
-        }
-    as.numeric(add_delta)
+  if (length(add_delta) > 1) {
+    stop("'add_delta' has to be a single value, either logical or numeric")
+  }
+  if (!(is.logical(add_delta) || is.numeric(add_delta))) {
+    stop("'add_delta' has to be either logical or numeric")
+  }
+  if (is.numeric(add_delta) && add_delta < 0) {
+    stop("'add_delta' has to be zero or a positive number")
+  }
+  as.numeric(add_delta)
 }
 
 #' @title Extract a MRF penalty matrix
@@ -186,25 +199,28 @@
 #'
 #' @export
 `get_penalty` <- function(penalty, ...) {
-    UseMethod("get_penalty")
+  UseMethod("get_penalty")
 }
 
 #' @export
 `get_penalty.default` <- function(penalty, ...) {
-    ## want to bail with a useful error;
-    ## see Jenny Bryan's Code Smells UseR 2018 talk: rstd.io/code-smells
-    stop("Don't know how to extract a penalty matrix from <",
-         class(penalty)[[1L]], ">",
-         call. = FALSE)           # don't show the call, simpler error
+  ## want to bail with a useful error;
+  ## see Jenny Bryan's Code Smells UseR 2018 talk: rstd.io/code-smells
+  stop(
+    "Don't know how to extract a penalty matrix from <",
+    class(penalty)[[1L]],
+    ">",
+    call. = FALSE
+  ) # don't show the call, simpler error
 }
 
 #' @rdname get_penalty
 #'
 #' @export
 `get_penalty.mrf_penalty` <- function(penalty, ...) {
-    attr(penalty, "mrf_config") <- NULL
-    class(penalty) <- "matrix"
-    penalty
+  attr(penalty, "mrf_config") <- NULL
+  class(penalty) <- "matrix"
+  penalty
 }
 
 #' Convert a MRF penalty object to a matrix
